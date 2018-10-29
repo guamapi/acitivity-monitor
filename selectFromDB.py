ace8e2187e609b36cc6c23b61c29213b8939ecac
SHENGYU GUI <shengyugui@SHENGYUs-MBP.hsd1.ca.comcast.net>
2018-10-28 23:50:14 -0700

    add comment

--- selectFromDB.py
+++ selectFromDB.py
@@ -5,19 +5,19 @@ class readDB(object):
         self.file_path = file_path
         self.conn = sqlite3.connect(self.file_path)
         self.c = self.conn.cursor()
-
+    #return keyboard input
     def getKeyboard():
         for row in c.execute('select * from kt'):
             return row
-
+    #return mouse click
     def getMouse():
         for row in c.execute('select * from ml'):
             return row
-
+    #return mouse map
     def getMousemap():
         for row in c.execute('select * from mousemap'):
             return row
-
+    #return window name
     def getWindowname():
         for row in c.execute('select * from mw'):
             print row
